980a214cf06061cd00dbbc0f3877a2e5f8097eb9
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-02-09 16:05:05 +0300

    complete assigmnet

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const num3 = 40;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+const max = Math.max(20, 10);
